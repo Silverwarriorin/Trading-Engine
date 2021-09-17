f2dbd00e760b6f79dc2cf94e7ef8ea645c0f610f
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2021-09-17 10:35:34 -0400

    its happening

---
+++ TradingGameEngine/src/Monitary_System.java
@@ -0,0 +1,15 @@
+public class Monitary_System {
+
+
+
+
+    public boolean TryTrade(String Item)
+    {
+
+    }
+
+    public int CalcPrice(int AmountSold)
+    {
+        return -1*AmountSold;
+    }
+}
