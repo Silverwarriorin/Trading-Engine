c6b4e49112fe883547a622cb9524872ded95d414
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2021-09-17 13:05:29 -0400

    egg

--- TradingGameEngine/src/Monitary_System.java
+++ TradingGameEngine/src/Monitary_System.java
@@ -1,15 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Monitary_System {
 
 
 
+    public int SellItem(int AmountSold, String item, String port) throws FileNotFoundException {
 
-    public boolean TryTrade(String Item)
-    {
+        File file2 =
+                new File("src/Ports/"+port+".portdata");
+        Scanner por = new Scanner(file2);
 
-    }
+        if(por.hasNext(item))
+        {
+            File file =
+                    new File("src/Items/"+item+".item");
+            Scanner sc = new Scanner(file);
 
-    public int CalcPrice(int AmountSold)
-    {
-        return -1*AmountSold;
+            return AmountSold * sc.nextInt();
+        }
+        else{
+            return 0;
+        }
+
     }
 }
+
+
